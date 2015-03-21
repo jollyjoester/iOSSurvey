059def97f6aa7cf1477e517f95eb749202a208aa
jollyjoester <jollyjoester@gmail.com>
2015-03-21 11:26:14 +0900

    Add: initial project

---
+++ iOSSurvey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSSurvey
+//
+//  Created by jollyjoester_pro on 3/21/15.
+//  Copyright (c) 2015 Hideyuki Nanashima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSSurvey/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSSurvey
+//
+//  Created by jollyjoester_pro on 3/21/15.
+//  Copyright (c) 2015 Hideyuki Nanashima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
